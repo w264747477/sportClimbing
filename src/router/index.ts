3ee92bbae5a331913fe91fa2e524783088b5b36d
wangqq77@ <wangqiqi@cmiot.cmcc>
2023-02-28 16:37:27 +0800

    test(custom): 测试路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,36 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+const Home = () => import("@/views/home.vue");
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: '',
-        redirect: '/hello',
+  {
+    path: "",
+    redirect: "/hello",
+  },
+  {
+    path: "/",
+    redirect: "/hello",
+  },
+  {
+    path: "/hello",
+    name: "HelloWorld",
+    meta: {
+      title: "hello",
+      keepAlive: true,
+      requireAuth: false,
     },
-    {
-        path: '/',
-        redirect: '/hello',
+    component: HelloWorld,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    meta: {
+      title: "home",
     },
-    {
-        path: '/hello',
-        name: 'HelloWorld',
-        meta: {
-            title: 'hello',
-            keepAlive: true,
-            requireAuth: false,
-        },
-        component: HelloWorld,
-    },
+    component: Home,
+  },
 ];
 const router = createRouter({
-    history: createWebHistory('/sportClimbing'),
-    routes,
+  history: createWebHistory("/sportClimbing"),
+  routes,
 });
 export default router;
